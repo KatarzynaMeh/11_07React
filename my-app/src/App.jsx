6aa9afd6548396d3b0e8eb4fc25947b39d826b5c
Mekhova Kateryna <katrindeutschland33333@gmail.com>
2024-01-11 13:00:41 +0100

    2 commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { AboutMe } from "./components/AboutMe";
+
+export function App() {
+  return (
+    <>
+      <div className="App">Hello World</div>
+      <p>My name is Katia</p>
+      <AboutMe />
+    </>
+  );
+}
+
+// export default App;
---
+++ my-app/src/components/AboutMe.jsx
@@ -0,0 +1,5 @@
+export const AboutMe = () => { 
+    return <div>
+        <h1>I am frontend developer</h1>
+    </div>
+}
